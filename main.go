01644d9cad10173aa61258fcd3786dfd66f48380
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2020-01-10 14:00:24 -0800

    first steps

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 const message = "Hello World"
 
 func main() {
-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(message))
+	})
+	http.ListenAndServe(":8080", mux)
 	fmt.Println("Hello world")
 }
