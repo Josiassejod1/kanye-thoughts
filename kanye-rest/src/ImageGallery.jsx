38a3e474dd6b159b94b89da1ec20ed3cc9c77c47
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2020-01-23 16:47:41 -0800

    create text area for app

--- kanye-rest/src/ImageGallery.jsx
+++ kanye-rest/src/ImageGallery.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import imageCompression from 'browser-image-compression';
 import html2canvas from 'html2canvas';
-
+import {Editor, EditorState} from 'draft-js';
+import Toolbar from 'draft-js-buttons';
 class ImageGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: 'album-1.png'
+      display: 'album-1.png',
+      editorState: EditorState.createEmpty()
     };
+    this.setEditor = (editor) => {
+      this.editor = editor;
+    }
+    this.onChange = (editorState) => this.setState({editorState});
   }
 
   handleSelect(e){
@@ -52,26 +58,52 @@ class ImageGallery extends React.Component {
       "album-8.png",
     ]
     var src = "kanye/"
+    const styles = {
+      editor: {
+        boxSizing: 'border-box',
+        border: '1px solid #ddd',
+        cursor: 'text',
+        padding: '16px',
+        borderRadius: '2px',
+        marginBottom: '2em',
+        boxShadow: 'inset 0px 1px 8px -3px #ABABAB',
+        background: '#fefefe',
+        height: '100px',
+      }
+    };
     var quote = this.props.quote;
     var selectedImage = this.state.display;
+    var text = this.state.editorState.getCurrentContent().getPlainText();
     return(
     <React.Fragment>
     <div id="capture"
-
       style={{
       position: 'relative',
       textAlign: 'center',
     }}>
       <img width="500" height="500" src={src + selectedImage} />
-      <h1 style={{
+      <textarea defaultValue={quote} placeHolder="Text Here" style={{
+        fontSize: 100,
+        resize: 'none',
+        margin: '0 auto',
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
         overflow: 'hidden'
-      }}>{quote}</h1>
+      }}></textarea>
     </div>
+
     <input name="Download" type="image" src="kanye/download.png" onClick={(e) => this.handleDownload()} />
+    <div style={styles.editor}>
+    <Editor
+      ref={this.setEditor}
+      editorState={this.state.editorState}
+      onChange={this.onChange}
+      />
+    </div>
     <div data-html2canvas-ignore style={{padding: '20px'}}>
       {
         image_array.map((image, i) => (
